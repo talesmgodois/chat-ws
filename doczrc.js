00e2c596eac7854a1e86d448e9b0ff6253396637
Tales Marinho Godois <tales.godois@gmail.com>
2019-03-28 19:01:27 -0300

    adding changes

--- doczrc.js
+++ doczrc.js
@@ -1,4 +1,19 @@
 export default {
   title: "Chat ws",
   description: "This is my awesome documentation",
+  themeConfig: {
+    mode: "dark",
+    codemirrorTheme: "dracula",
+    styles: {
+      playground: {
+        padding: ["0", "32px"],
+        height: "100%",
+        background: "#fff",
+      },
+      h1: {
+        fontFamily: "'Source Sans Pro', Helvetica, sans-serif",
+        fontSize: 36,
+      },
+    },
+  },
 };
