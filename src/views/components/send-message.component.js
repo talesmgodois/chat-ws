bdc2fa17779799aacc6b2f727c359a94089566ca
Tales Marinho Godois <tales.godois@gmail.com>
2019-05-03 18:50:41 -0300

    colocando style

--- src/views/components/send-message.component.js
+++ src/views/components/send-message.component.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { withStyles } from "@material-ui/core";
 import Fab from "@material-ui/core/Fab";
-import FormControl from "@material-ui/core/FormControl";
 import Grid from "@material-ui/core/Grid";
 import InputBase from "@material-ui/core/InputBase";
 import SendIcon from "@material-ui/icons/Send";
@@ -17,16 +16,19 @@ const SendMessageComponent = props => {
   return (
     <Grid container className={classes.box} spacing={24}>
       <Grid item xs={10}>
-        <FormControl className={classes.margin}>
-          <InputBase
-            id="bootstrap-input"
-            defaultValue="react-bootstrap"
-            classes={{
-              root: classes.bootstrapRoot,
-              input: classes.bootstrapInput,
-            }}
-          />
-        </FormControl>
+        <Grid container alignItems="stretch" justify="flex-start">
+          <Grid item xs={12}>
+            <InputBase
+              fullWidth={true}
+              id="bootstrap-input"
+              placeholder="Type your text here"
+              classes={{
+                root: classes.bootstrapRoot,
+                input: classes.bootstrapInput,
+              }}
+            />
+          </Grid>
+        </Grid>
       </Grid>
       <Grid item xs>
         <Fab color="primary" aria-label="Add">
