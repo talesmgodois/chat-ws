0b6de896172090437193212f0638acd56732db16
Tales Marinho Godois <tales.godois@gmail.com>
2019-05-03 15:39:20 -0300

    message ready

--- src/views/components/message.component.js
+++ src/views/components/message.component.js
@@ -1,37 +1,54 @@
-import React from 'react';
-import { Typography, Card, CardContent } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import IconButton from '@material-ui/core/IconButton';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import { Typography, Card } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import { withStyles } from "@material-ui/core/styles";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 const styles = {
-    card: {
-        backGroundColor: 'red',
-        padding: 10
-    }
+  card: {
+    backGroundColor: "red",
+    padding: 10,
+  },
+  body: {
+    width: "90%",
+  },
+};
+
+const messageButton = () => {
+  return (
+    <Grid item xs={2}>
+      <IconButton>
+        <MoreVertIcon />
+      </IconButton>
+    </Grid>
+  );
 };
 
 const MessageComponent = props => {
-    const {
-        msg: { content },
-        classes
-    } = props;
-    return (
-        <Card>
-            <Grid container>
-                <Grid item xs={10}>
-                    <Typography className={classes.card}>{content}</Typography>
-                </Grid>
+  const { msg: { content }, classes, type } = props;
+  const leftButton = type === "left" ? messageButton() : null;
+  const leftSpace = type === "left" ? <Grid item xs={2} /> : null;
+  const rightButton = type === "right" ? messageButton() : null;
+  const rightSpace = type === "right" ? <Grid item xs={2} /> : null;
 
-                <Grid item xs={2}>
-                    <IconButton>
-                        <MoreVertIcon />
-                    </IconButton>
-                </Grid>
+  return (
+    <Grid container>
+      {leftSpace}
+      <Grid item xs={10}>
+        <Card>
+          <Grid container>
+            {rightButton}
+            <Grid item xs={10}>
+              <Typography className={classes.card}>{content}</Typography>
             </Grid>
+            {leftButton}
+          </Grid>
         </Card>
-    );
+      </Grid>
+      {rightSpace}
+    </Grid>
+  );
 };
 
 export default withStyles(styles)(MessageComponent);
--- stories/message.js
+++ stories/message.js
@@ -1,32 +1,34 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
+import React from "react";
+import Message from "../src/domain/message.domain";
 
-import Message from '../src/domain/message.domain';
-import MessageComponent from '../src/views/components/message.component';
+import MessageComponent from "../src/views/components/message.component";
+import { storiesOf } from "@storybook/react";
 
-storiesOf('Message Default', module)
-    .add('Message rendering', () => {
-        const msg = new Message({ content: 'Hi how are you?' });
-        return <MessageComponent msg={msg} />;
-    })
-
-    .add('Message other size', () => {
-        const msg = new Message({ content: 'Hi how are you?' });
-        return (
-            <div style={{ width: 200 }}>
-                <MessageComponent msg={msg} />
-            </div>
-        );
-    })
-
-    .add('Long message', () => {
-        const msg = new Message({
-            content:
-                'Hi how are you?Hi how are you?Hi how are you?Hi how are you?Hi how are you?Hi how are you?Hi how are you?Hi how are you?Hi how are you?Hi how are you?Hi how are you?'
-        });
-        return (
-            <div style={{ width: 320 }}>
-                <MessageComponent msg={msg} />
-            </div>
-        );
+storiesOf("Message Default", module)
+  .add("Message rendering", () => {
+    const msg = new Message({ content: "Hi how are you?" });
+    return <MessageComponent msg={msg} />;
+  })
+  .add("Message other size", () => {
+    const msg = new Message({ content: "Hi how are you?" });
+    return (
+      <div style={{ width: 200 }}>
+        <MessageComponent msg={msg} />
+      </div>
+    );
+  })
+  .add("Long message", () => {
+    const msg = new Message({
+      content:
+        "Hi how are you?Hi how are you?Hi how are you?Hi how are you?Hi how are you?Hi how are you?Hi how are you?Hi how are you?Hi how are you?Hi how are you?Hi how are you?",
     });
+    return (
+      <div style={{ width: 320 }}>
+        <MessageComponent msg={msg} />
+
+        <MessageComponent msg={msg} type={"left"} />
+
+        <MessageComponent msg={msg} type={"right"} />
+      </div>
+    );
+  });
