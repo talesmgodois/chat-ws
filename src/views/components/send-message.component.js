673cf204864eb02cb4830b76c030558f120c7144
Tales Marinho Godois <tales.godois@gmail.com>
2019-05-08 14:24:13 -0300

    Using proptypes on Send-message component, and changing placeholder and action using props

--- src/views/components/send-message.component.js
+++ src/views/components/send-message.component.js
@@ -1,3 +1,4 @@
+import { PropTypes } from "prop-types";
 import React from "react";
 import { withStyles } from "@material-ui/core";
 import Fab from "@material-ui/core/Fab";
@@ -12,31 +13,41 @@ const styles = {
 };
 
 const SendMessageComponent = props => {
-  const { classes } = props;
+  const logIt = (params = "default click") => {
+    console.log(params);
+  };
+  const {
+    classes,
+    handleSendButtonClick = logIt,
+    placeholder = "Type your text here",
+  } = props;
+
   return (
     <Grid container className={classes.box} spacing={24}>
       <Grid item xs={10}>
         <Grid container alignItems="stretch" justify="flex-start">
           <Grid item xs={12}>
-            <InputBase
-              fullWidth={true}
-              id="bootstrap-input"
-              placeholder="Type your text here"
-              classes={{
-                root: classes.bootstrapRoot,
-                input: classes.bootstrapInput,
-              }}
-            />
+            <InputBase fullWidth={true} placeholder={placeholder} />
           </Grid>
         </Grid>
       </Grid>
       <Grid item xs>
-        <Fab color="primary" aria-label="Add">
+        <Fab
+          color="primary"
+          aria-label="Add"
+          onClick={() => handleSendButtonClick()}
+        >
           <SendIcon />
         </Fab>
       </Grid>
     </Grid>
   );
+};
+
+SendMessageComponent.propTypes = {
+  classes: PropTypes.object,
+  handleSendButtonClick: PropTypes.func,
+  placeholder: PropTypes.string,
 };
 
 export default withStyles(styles)(SendMessageComponent);
--- stories/send-message.js
+++ stories/send-message.js
@@ -3,6 +3,21 @@ import React from "react";
 import SendMessageComponent from "../src/views/components/send-message.component";
 import { storiesOf } from "@storybook/react";
 
-storiesOf("Send Message", module).add("Send message rendering", () => {
-  return <SendMessageComponent />;
-});
+storiesOf("Send Message", module)
+  .add("Send message rendering", () => {
+    return <SendMessageComponent />;
+  })
+  .add("Change click action", () => {
+    const click = () => {
+      console.log("new click baby");
+    };
+    return <SendMessageComponent handleSendButtonClick={click} />;
+  })
+  .add("Changing placeholder", () => {
+    return (
+      <React.Fragment>
+        <SendMessageComponent placeholder={"Cool placehholder"} />
+        <SendMessageComponent placeholder={"Another placehholder"} />
+      </React.Fragment>
+    );
+  });
